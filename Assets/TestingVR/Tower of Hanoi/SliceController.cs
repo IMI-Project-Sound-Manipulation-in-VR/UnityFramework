ca9ede8da1ee99db442c987ce881bffccbcca406
Crimson Snow <juri.wiechmann@gmx.net>
2023-05-02 00:45:55 +0200

    Tower of Hanoi

---
+++ Assets/TestingVR/Tower of Hanoi/SliceController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace TestingVR.Tower_of_Hanoi
+{
+    [RequireComponent(typeof(XRGrabInteractable))]
+    public class SliceController : MonoBehaviour
+    {
+        private XRGrabInteractable _grabInteractable;
+
+        public XRGrabInteractable GrabInteractable
+        {
+            get => _grabInteractable;
+            set => _grabInteractable = value;
+        }
+
+
+        private void Awake()
+        {
+            _grabInteractable = GetComponent<XRGrabInteractable>();
+        }
+
+        public void DisableGrab()
+        {
+            _grabInteractable.enabled = false;
+        }
+
+        public void EnableGrab()
+        {
+            _grabInteractable.enabled = true;
+        }
+
+        public bool IsInGrab()
+        {
+            
+            return _grabInteractable.isSelected;
+        }
+
+        public UnityEvent<SelectEnterEventArgs> GetOnGrabEvent()
+        {
+            return _grabInteractable.selectEntered;
+        }
+    }
+}
