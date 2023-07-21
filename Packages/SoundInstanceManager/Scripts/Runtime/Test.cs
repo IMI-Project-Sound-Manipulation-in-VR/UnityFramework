45a6b11528aaff74c2368441d9e1a319dc8af735
Ruslan Novikov <ruslan.novikov@hotmail.de>
2023-07-21 15:40:49 +0200

    added sim to project

--- Packages/SoundInstanceManager/Scripts/Runtime/Test.cs
+++ Packages/SoundInstanceManager/Scripts/Runtime/Test.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     // fields should match parameter name to be automatically used by sound instance manager
-    [SerializeField] private float cover;
-    [SerializeField] private float rain;
-    [SerializeField] private float wind;
+    [SerializeField] private float traffic;
+    public float Traffic { get { return traffic; } set { traffic = value; } }
+    [SerializeField] private float walla;
+    public float Walla { get { return walla; } set { walla = value; } }
     private float minValue = 0f;
     private float maxValue = 1f;
 
@@ -18,8 +19,7 @@ public class Test : MonoBehaviour
 
     private void UpdateValue()
     {
-        cover = Random.Range(minValue, maxValue);
-        rain = Random.Range(minValue, maxValue);
-        wind = Random.Range(minValue, maxValue);
+        traffic = Random.Range(minValue, maxValue);
+        Walla = Random.Range(minValue, maxValue);
     }
 }
